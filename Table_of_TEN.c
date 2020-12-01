68b56b93f2b96de31c244ac8da41b20d0ac47a2e
aakashgoel2002 <65633502+aakashgoel2002@users.noreply.github.com>
2020-12-02 01:09:39 +0530

    Update Table_of_TEN.c

--- Table_of_TEN.c
+++ Table_of_TEN.c
@@ -4,8 +4,8 @@ int main()
     int i, sum=0, n;
     printf("Enter the number of which the table is to be printed: ");
     scanf("%d",&n);
-    for(i=1;i<=10;i++)
+    for(i=1;i<=10;i++) //loop for printing table
     {
-        printf("\n%d * %d = %d",n,i,(n*i));
+        printf("\n%d * %d = %d",n,i,(n*i)); //formatted so as to print Ex: "2 * 2 = 4"
     }
 }
