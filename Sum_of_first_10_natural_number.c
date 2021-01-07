17bd005615a72678fa5310ffa9c9bce2371bb656
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-07 19:33:46 +0530

    Sum_of_first_10_natural_number.c

--- Sum_of_first_10_natural_number.c
+++ Sum_of_first_10_natural_number.c
@@ -2,13 +2,11 @@
 //importing package
 int main(){
     int i=1, sum=0, n=10;
- 
     while( i<=n){ //loop for sum of the first 10 natural numbers
         sum +=i;
         i++; 
     }
-    printf("The value of sum(1 to 10) is %d", sum);
-    //printing the sum
+    printf("The value of sum(1 to 10) is %d", sum);//printing the sum
 
     for(i=1;i<=10;i++)
     sum+=i;
