79b80c14fbd596a13a5f9d51676969ada8a3815d
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-08 23:48:47 +0530

    area_of_square_using_function.c

--- area_of_square_using_function.c
+++ area_of_square_using_function.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int side;
-  //  float area = pow(side, 2);
+    int side;   //float area = pow(side, 2);
     printf("enter the side of the square\n");
     scanf("%d", &side);
     printf("the area of the square is %f", pow(side,2));
