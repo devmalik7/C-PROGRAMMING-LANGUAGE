a234bd6f347642e2dd1735dbcdedd7d3db1a1a12
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2021-01-09 02:43:45 +0530

    Delete area_of_square_using_function.c

--- area_of_square_using_function.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int s;
-    printf("Enter the length of a side of the square: ");
-    scanf("%d", &s);
-    printf("The area of the square is: %f", pow(s,2));
-    return 0;
-}
