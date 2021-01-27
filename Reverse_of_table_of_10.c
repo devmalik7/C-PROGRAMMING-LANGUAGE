6b233101812137ca2da029a249f67c2b34e2811c
devmalik7 <devm333666@gmail.com>
2021-01-27 23:50:57 +0530

    Update and rename Reverse_of_table.c to Reverse_of_table_of_10.c

--- Reverse_of_table_of_10.c
+++ Reverse_of_table_of_10.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-    printf("****Multiplication table of 10****\n\n");
+    printf("Multiplication table of 10 \n\n");
     for(int i=10;i>=0;i--)
     {
         printf("10 X %d = %d\n", i, 10*i);
