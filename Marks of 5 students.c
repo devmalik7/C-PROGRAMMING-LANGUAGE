e8cde7ba8459face03d1273f9ce3fd1859f31028
devmalik7 <devm333666@gmail.com>
2021-03-17 23:07:09 +0530

    Update Marks of 5 students.c

--- Marks of 5 students.c
+++ Marks of 5 students.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    int i=5;
+    int mark[5];
+    for(i=0;i<5;i++)
+    {
+         printf("Enter the  marks of  %d:",i+1);
+         scanf("%d", &mark[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("The markes of %d is %d\n", i+1 ,mark[i]);
+    }
+    return 0; 
+}
